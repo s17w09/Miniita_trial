283519249333c574f493f02533483b31dab533c7
s17w09 <s0917.smys04@gmail.com>
2024-06-08 21:46:12 +0900

    ユーザー情報をDBへ保存成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
 
     if @user.save
-      redirecto_to root_path
+      redirect_to root_path
     else
       render :new
+      flash[:notice] = '新規登録に失敗しました'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
