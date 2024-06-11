b872dee97413a2149a46c6ba98798fd1ca0a0aa1
s17w09 <s0917.smys04@gmail.com>
2024-06-11 22:18:24 +0900

    ログインしているユーザーでのみ編集削除ボタン配置

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+
+  def created_by?(user)
+    return true if user_id == user.id
+  end
 end
