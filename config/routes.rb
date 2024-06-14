3b23391da296419ba0d63928b09f542c92e38bff
s17w09 <s0917.smys04@gmail.com>
2024-06-14 21:48:01 +0900

    チャット機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :articles
   resources :templates, only: [:new]
-  resources :miniitabots, only: [:create, :new]
   
+  namespace :openai do
+    get 'tests/', to: 'tests#generate_text'
+    get 'tests/show', to: 'tests#show'
+  end
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   delete 'logout' => 'user_sessions#destroy', :as => :logout
