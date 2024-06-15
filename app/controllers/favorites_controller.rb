c0b35aff415e7ad66f40d219c5ac735fb98de66a
s17w09 <s0917.smys04@gmail.com>
2024-06-15 20:18:48 +0900

    ツールチップの追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    @first_favorite = current_user.favorites.new(article_id: params[:article_id])
-    if @first_favorite.save
+    @favorite = current_user.favorites.new(article_id: params[:article_id])
+    if @favorite.save
       redirect_to article_path(params[:article_id])
     end
   end
 
   def destroy
-    @first_favorite = current_user.favorites.find_by(article_id: params[:article_id])
-    if @first_favorite.destroy!
+    @favorite = current_user.favorites.find_by(article_id: params[:article_id])
+    if @favorite.destroy!
       redirect_to article_path(params[:article_id]), status: :see_other
     end
 end
