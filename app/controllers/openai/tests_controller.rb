9e02ffb7dc296679d21deadcf4222f8f147a60b9
s17w09 <s0917.smys04@gmail.com>
2024-06-15 15:01:57 +0900

    チャット機能の修正

--- app/controllers/openai/tests_controller.rb
+++ app/controllers/openai/tests_controller.rb
@@ -1,9 +1,7 @@
 class Openai::TestsController < ApplicationController
         protect_from_forgery
       
-        def show
-      
-        end
+        def show; end
       
         def generate_text
           @user_input = params[:user_input]
@@ -12,14 +10,17 @@ class Openai::TestsController < ApplicationController
             client = OpenAI::Client.new
             response = client.chat(
               parameters: {
-                model: "gpt-3.5-turbo",
+                model: "gpt-4o",
                 messages: [
-                  { role: "system", content: "関西弁で返答してください" },
+                  { role: "system", content: "あなたは優秀なエンジニアです。初心者エンジニアでもわかるように、userの質問に回答してください" },
                   { role: "user", content: @user_input }
                 ],
-                temperature: 0.7
+                # 生成するテキストの創造性やランダム性を制御するパラメータ。値が低いと、より決定論的な（予測可能な）応答を生成し、値が高いと、より多様で創造的な応答を生成する
+                temperature: 0.2
               }
             )
+
+            # digメソッドは、ネストされたハッシュや配列から値を安全に取得するための便利なメソッド。キーが存在しない場合でもエラーを出さずにnilを返すため、ネストが深いデータ構造を扱う際に役立つ。
             @generated_text = response.dig("choices", 0, "message", "content")
             render json:{text: @generated_text}
           end
