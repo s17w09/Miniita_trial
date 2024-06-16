6be60b3c2d5808eb28b94aeafc63eaa8feb4f756
s17w09 <s0917.smys04@gmail.com>
2024-06-16 18:09:08 +0900

    見出し語の対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,10 +20,21 @@ module ApplicationHelper
     def markdown(text)
       html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
       options = {
-        autolink: true,
-        space_after_headers: true,
+        autolink: true,  # URLやメールアドレスを自動的にリンクに変換、gyazo,通常のリンクテキスト対応
+        space_after_headers: true, #見出し後の文字の大きさ変換。app/assets/stylesheets/redcarpet_custom.cssでCSS指定し、app/assets/config/manifest.jsとapp/views/layouts/application.html.erbで指定。
         fenced_code_blocks: true,
+        tables: true,
+        hard_wrap: true,
+        xhtml: true,
+        lax_html_blocks: true, # HTMLブロックを緩やかに解析するオプション。区切り線は通常のredcarpetにも入っているが、マークダウンとHTMLの混在をスムーズに処理してくれる
+        no_intra_emphasis: true, #語句の強調。「**強調**」で強調される。（**の後にスペースなし）
+        strikethrough: true # 打ち消し線（ストライクスルー）。「~~打ち消し線~~」で打ち消し線が入る
       }
+
+      extensions = {
+      fenced_code_blocks: true
+    }
+
       markdown    = Redcarpet::Markdown.new(html_render, options)
       markdown.render(text)
     end
