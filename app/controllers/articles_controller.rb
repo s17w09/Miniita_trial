77fd9dc270ed6a7a3db053fa19881f6836c99057
s17w09 <s0917.smys04@gmail.com>
2024-06-21 17:41:41 +0900

    ダークモード対応

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  skip_before_action :require_login, only: [:index, :show]
+  skip_before_action :require_login
 
   def index
     @q = Article.ransack(params[:q])
